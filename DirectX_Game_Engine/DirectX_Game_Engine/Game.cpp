321d3ec8a3d01ae0b51f25f7ff4609a567beb377
LJPCODESTUDIO <89280590+LJPCODESTUDIO@users.noreply.github.com>
2023-05-24 10:35:34 -0600

    HE CAN MOVE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- DirectX_Game_Engine/DirectX_Game_Engine/Game.cpp
+++ DirectX_Game_Engine/DirectX_Game_Engine/Game.cpp
@@ -6,15 +6,33 @@
 Game::Game(MainWindow& _window) : window(_window), gui(_window) {}
 
 void Game::game_loop() {
+	if (initial) {
+		start_time = clock();
+		initial = false;
+		track_1.Play();
+	}
+	check_time_and_fps(tick_60);
+	if (game_time - track_time >= 192) {
+		track_time = game_time;
+		track_1.Play();
+	}
 	if (start) {
 		start_game();
 		return;
 	}
-	track_1.Play();
+
 	gui.set_Target_SubGui(1, 0);
 	gui.set_RGB_on_Gui(black);
 
-	write("Score: ", 0, 0, "chars_small");
+	player_image = 0;
+	keyboard_key_press();
+
+	gui.set_Image_at_Pixel(player_pos[0], player_pos[1], player[player_image]);
+
+	write("Score: 0", 0, 0, "chars_small");
+	write("Time: " + std::to_string(game_time), 0, 25, "chars_small");
+	write("FPS: " + std::to_string(fps) + " High: " + std::to_string(high_low_fps[0]) + " Low: " + std::to_string(high_low_fps[1]), 0, 50, "chars_small");
+	write("Pos: " + std::to_string(player_pos[0]) + "," + std::to_string(player_pos[1]), 0, 75, "chars_small");
 
 	gui.refresh_Gui();
 
@@ -50,6 +68,42 @@ void Game::write(std::string input, int start_x, int start_y, std::string font) 
 		else if (font == "chars_small") {
 			gui.set_Image_at_Pixel(start_x + x_offset, start_y, chars_small[chars_map.find_first_of(input[i])]);
 			x_offset += 15;
+		}
+	}
+}
+
+void Game::check_time_and_fps(float last_tick_60) {
+	current_time = clock() - start_time;
+	game_time = ((double)current_time) / CLOCKS_PER_SEC;
+	tick_60 = game_time * 60;
+	delta = tick_60 - last_tick_60;
+	fps = round(60 / delta);
+	if (fps > high_low_fps[0]) {
+		high_low_fps[0] = fps;
+	}
+	else if (fps < high_low_fps[1] && fps >= 0) {
+		high_low_fps[1] = fps;
+	}
+}
+
+void Game::keyboard_key_press() {
+	if (gui.get_Gui_Elem() == 1) {
+		int dir_x = window.kbd.KeyIsPressed(0x44) - window.kbd.KeyIsPressed(0x41);
+		int dir_y = window.kbd.KeyIsPressed(0x53) - window.kbd.KeyIsPressed(0x57);
+		int dash = window.kbd.KeyIsPressed(VK_SPACE) * 10;
+		player_pos[0] += dir_x * (7 + dash);
+		player_pos[1] += dir_y * (7 + dash);
+		if (player_pos[0] >= 995) {
+			player_pos[0] -= 7 + dash;
+		}
+		if (player_pos[0] <= 0) {
+			player_pos[0] += 7 + dash;
+		}
+		if (player_pos[1] >= 870) {
+			player_pos[1] -= 7 + dash;
+		}
+		if (player_pos[1] <= 0) {
+			player_pos[1] += 7 + dash;
 		}
 	}
 }
--- DirectX_Game_Engine/DirectX_Game_Engine/Game.h
+++ DirectX_Game_Engine/DirectX_Game_Engine/Game.h
@@ -4,11 +4,14 @@
 #include "Sound.h"
 #include "Image.h"
 #include "Sprite.h"
+#include <time.h>
 
 class Game {
 private:
 	MainWindow& window;
 	Gui gui;
+
+	clock_t start_time, current_time;
 
 	//colors for images and sprites
 	uint8_t white[3] = { 255,255,255 };
@@ -19,25 +22,41 @@ private:
 	uint8_t blue[3] = { 0, 165, 255 };
 	uint8_t pink[3] = { 255,0,231 };
 	uint8_t green[3] = { 186, 254, 202 };
-	uint8_t green_screen[3] = { 0,5,0 };
+	uint8_t green_screen[3] = { 0,10,0 };
 
+	Sprite player = Sprite(2, "Images/Player.bmp", green_screen, nullptr, 3, 1);
 	Image funky_chars = Image("Images/funky_chars.bmp", white, red, 15, 8);
-	Image chars_small = Image("Images/chars.bmp", white, red, 32, 3);
+	Image chars_small = Image("Images/chars.bmp", white, white, 32, 3);
 
 	Sound track_1 = Sound(L"./Sounds/Hiding Your Reality.wav");
 
 	Sprite play = Sprite(1, "Images/Play_Button.bmp", green_screen, nullptr);
+	
 
 	std::string chars_map = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_'abcdefghijklmnopqrstuvwxyz{|}~ ";
 
 	bool start = true;
+	bool initial = true;
 
 	int WIDTH = 1024;
 	int HEIGHT = 900;
+	int fps;
+	int high_low_fps[2] = { 60,60 };
+	int player_pos[2] = { 512, 450 };
+	int player_image = 0;
+
+	float game_time;
+	float track_time = 0;
+	float tick_60 = 0;
+	float delta;
 
 	void start_game();
 
 	void write(std::string input, int start_x, int start_y, std::string font = "funky_chars");
+
+	void check_time_and_fps(float last_tick_60);
+
+	void keyboard_key_press();
 
 public:
 	Game(class MainWindow& _app_window);
